57d3e76d1215597ac79c14017c8ae52e0040e42e
NikolajManssonEsSolutions <nikolaj.mansson@essolutions.se>
2022-01-27 18:48:53 +0100

    Listing items added to cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,43 @@
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Cart extends Component{
 
     render(){
+
+        let addedItems = this.props.items.length ?
+        (
+            this.props.items.map(item=>{
+                return(
+                    <li className="collection-item avatar" key={item.id}>
+                        <p>{item.department} {item.name}: {item.price} kr</p>
+                        <button className="waves-effect waves-light btn pink remove">Ta bort</button>
+                    </li>
+                )
+            })
+        ):
+        (
+            <p>Inga sparade tillval</p>
+        )
+
         return(
-            <div><p>Hello from cart</p></div>
-        );
+            <div className="container">
+                <p>Din offert</p>
+                <ul>
+                    {addedItems}
+                </ul>                
+            </div>
+        )
+        
     }
 }
 
-export default Cart;
+const mapStateToProps = (state) => {
+    return{
+        items: state.addedItems
+    }
+}
+
+export default connect(mapStateToProps)(Cart);
