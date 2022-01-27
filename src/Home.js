36cdc835fb287aea88114f249c3d771e42ed0209
NikolajManssonEsSolutions <nikolaj.mansson@essolutions.se>
2022-01-27 15:40:10 +0100

    List of items on Home-page

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,58 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Home extends Component{
 
     render(){
+        var item_HTML_TABLE = "";
+        item_HTML_TABLE = this.props.items.map(item=>{
+            return (
+                <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.department}</td>
+                    <td>{item.price}</td>
+                </tr>
+            )
+        })
 
         return(
             <div className="container">
-               <h3>Home</h3>
+               <div className="row">
+                    <div className="col-md-12">
+                        <div className="card">
+                            <div className="card-header">
+                                <h4>Erbjudanden</h4>
+                                    
+                            </div>
+                            <div className='card-body'>
+                                <table className="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Name</th>
+                                            <th>Department</th>
+                                            <th>Price</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {item_HTML_TABLE}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
         
 }    
 
+const mapStateToProps = (state) => {
+    return {
+        items: state.items
+    }
+}
 
-export default Home 
+export default connect(mapStateToProps)(Home) 
